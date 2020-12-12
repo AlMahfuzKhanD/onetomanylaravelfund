12d62c594f32f9765cfffb39fdef1dd54c9cbb2f
mahfuz380 <almahfuz380@gmail.com>
2020-12-13 00:03:57 +0600

    create and read done

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,24 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+use App\Post;
+use App\User;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/create',function(){
+ $user = User::findOrFail(1);
+$post = new Post(['title'=>'title 2','body'=>'content 2']);
+$user->posts()->save($post);
+});
+
+Route::get('/read', function(){
+$user = User::findOrFail(1);
+    foreach($user->posts as $post){
+        echo $post->body . "<br>";
+    }
+});
+
+Route::get
